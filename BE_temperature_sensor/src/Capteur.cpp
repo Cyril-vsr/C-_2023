8e40128f9e6f613e66f18f86e57452e8697f1e25
PauloElFamoso <paul.jaulhiac@gmail.com>
2023-12-13 09:33:12 +0100

    V0

---
+++ BE_temperature_sensor/src/Capteur.cpp
@@ -0,0 +1,46 @@
+﻿using namespace std;
+class Capteur
+{
+    Capteur() { }
+
+};
+
+class Temperature : public Capteur
+{
+    private:
+        char * name;
+        int temp;
+    public :
+        Temperature(char * nom){
+            *name = *nom;
+        }
+//Code à implémenter pour la com en i2c avec le sensor et affectation des valeurs lues dans temp :
+    ----------
+
+// Accessors :
+int getTemp()
+{
+    return temp;
+}
+void setTemp()
+{ 
+    temp = 0;
+}
+char * getName()
+{
+    return name;
+}
+void setName(string nom)
+{
+    name = nom;
+}
+
+}
+
+// Methods :
+void afficherTemperature()
+{
+    Capteur::afficherCaracteristiques();
+    cout << "Il fait " << getTemp() << "°C actuellement." << endl;
+}
+};
---
+++ BE_temperature_sensor/src/Capteur.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Capteur
+{
+    Capteur() { }
+
+}
+
+class Temperature : public Capteur
+{
+    private:
+        string name;
+        int temp;
+    public :
+        Temperature(string nom){
+            name = nom;
+        }
+//Code à implémenter pour la com en i2c avec le sensor et affectation des valeurs lues dans temp :
+    ----------
+
+// Accessors :
+int getTemp()
+{
+    return temp;
+}
+void setTemp()
+{ 
+    temp = 0;
+}
+string getName()
+{
+    return name;
+}
+void setName(string nom)
+{
+    name = nom;
+}
+
+}
+
+// Methods :
+virtual void afficherTemperature()
+{
+    Capteur::afficherCaracteristiques();
+    cout << "Il fait " << getTemp() << "°C actuellement." << endl;
+}
+};
